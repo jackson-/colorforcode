70a7adf794ea5499f059d326432dcf61820279cb
Chloe Rice <chloerice@users.noreply.github.com>
2017-05-22 13:30:51 -0400

    bugfix - employer instance methods
    Corrected some mismatched variable use after copying the functionality of the first instance method into the second. Removed unused/commented out code, and updated clarifying comments.

--- db/models/employer.js
+++ db/models/employer.js
@@ -2,7 +2,7 @@
 
 const {STRING} = require('sequelize')
 const db = require('APP/db')
-const {Job} = db
+const {Job, User} = db
 
 module.exports = db => db.define('employer', {
   name: STRING,
@@ -30,11 +30,11 @@ module.exports = db => db.define('employer', {
       })
     },
     addRecruiters: function(recruiterArray) {
-      const creatingNewRecruiters = listingsArray.length > 1
+      const creatingNewRecruiters = recruiterArray.length > 1
         ? User.bulkCreate(recruiterArray)
         : User.create(recruiterArray[0])
 
-      creatingNewJobs
+      creatingNewRecruiters
       .then(newRecruiters => {
         return this.getRecruiters()
         .then(oldRecruiters => {
@@ -52,85 +52,26 @@ module.exports = db => db.define('employer', {
   }
 })
 
-// Has Many associations add source id column to the target table.
-// In this case an employer id attribute is added to both the job and user tables.
-// Users instances will only have employers if they are recruiters that signed up
-// for an employer account (is_employer = true).
+// One to Many associations add a [source]_id column to the target table.
+// In this case an employer_id attribute is added to both the job and user tables.
+// User instances will only have employers if they signed up for an employer account (is_employer: true).
 
-//Employer instances have the accessor methods getListings/setListings as well as
-// getRecruiters and setRecruiters.
-
-module.exports.associations = (Employer, {Job, User}) => {
-  Employer.hasMany(Job, {as: 'Listings'})
-  Employer.hasMany(User, {as: 'Recruiters'})
-}
-
-// bcrypt docs: https://www.npmjs.com/package/bcrypt
-// const bcrypt = require('bcryptjs')
-// const {STRING, TEXT, JSON, VIRTUAL, INTEGER, ENUM, DATE, ARRAY} = require('sequelize')
-//
-// module.exports = db => db.define('employer', {
-//   first_name: {
-//       type: STRING,
-//       notEmpty: true
-//   },
-//   last_name: {
-//       type: STRING,
-//       notEmpty: true
-//   },
-//   company_name: STRING,
-//   image_url: STRING,
-//   email: {
-//     type: STRING,
-//     validate: {
-//         isEmail: true,
-//         notEmpty:true
-//     }
-//   },
-//   last_login: DATE,
-//   status: {
-//     type: ENUM('active', 'inactive'),
-//     defaultValue: 'active'
-//   },
-//   password: VIRTUAL // Note that this is a virtual, and not actually stored in DB
-// }, {
-//   indexes: [{fields: ['email'], unique: true}],
-//   hooks: {
-//     beforeCreate: setEmailAndPassword,
-//     beforeUpdate: setEmailAndPassword,
-//   }
-// })
-
-// Belongs to Many associations create a join table.
-
-// In this case we've defined JobApplication ourselves in order to add additional
-// attributes beyond the primary keys of the Job and Employer tables.
-
-// Both Job and Employer instances will have get, set and add accessor methods.
-// Employer instances have getApplications, setApplications, addApplication(s),
-// all of which return promises.
-// refer to http://docs.sequelizejs.com/en/v3/api/associations/belongs-to-many/
+// Employer instances have the accessor methods getListings/setListings as well as
+// getRecruiters/setRecruiters, all of which return promises.
+// refer to http://docs.sequelizejs.com/manual/tutorial/associations.html#one-to-many-associations
 
 /*
-   e.g., router.post('/:employerId/applications/:applicationId', (req, res, next) => {
+   e.g., router.post('/:employerId/jobs', (req, res, next) => {
            Employer.findById(req.params.employerId)
            .then(employer => {
-             employer.addApplication(req.params.applicationId) // takes an instance or its id
-             .then(application => res.sendStatus(201))
+             employer.addListings(req.body.jobs) // req.body.jobs is an array, see instance methods above
+             .then(listings => res.status(201).json(listings))
              .catch(next)
            })
          })
 */
 
-// module.exports.associations = (Employer, {OAuth, Job, JobApplication}) => {
-//   Employer.hasOne(OAuth)
-//   Employer.hasMany(Job)
-// }
-//
-// function setEmailAndPassword(employer) {
-//   employer.email = employer.email && employer.email.toLowerCase()
-//   if (!employer.password) return Promise.resolve(employer)
-//
-//   return bcrypt.hash(employer.get('password'), 10)
-//   .then(hash => employer.set('password_digest', hash))
-// }
+module.exports.associations = (Employer, {Job, User}) => {
+  Employer.hasMany(Job, {as: 'Listings'})
+  Employer.hasMany(User, {as: 'Recruiters'})
+}
