f9a7c5c91ca885382df48685a0ff19915c46925e
Chloe Rice <chloe.mi.rice@gmail.com>
2017-08-17 04:20:07 -0700

    svg icon system, chip component, user detail header, user model update

--- src/components/users/UserDetail.js
+++ src/components/users/UserDetail.js
@@ -1,115 +1,107 @@
 import React, { Component } from 'react'
 import { gettingUserById } from 'APP/src/reducers/actions/users'
-import { Row, Col, Image, Glyphicon } from 'react-bootstrap'
+import { Row, Col, Button, FormControl, ControlLabel, Image } from 'react-bootstrap'
 import blankAvatar from 'APP/src/components/dashboard/blank-avatar.png'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import IconBar from '../utilities/icons/IconBar'
-import TwitterIcon from '../utilities/icons/TwitterIcon'
-import LinkIcon from '../utilities/icons/LinkIcon'
-import GithubIcon from '../utilities/icons/GithubIcon'
-import LinkedInIcon from '../utilities/icons/LinkedInIcon'
-import Chips from '../utilities/chips/Chips'
-import './UserDetail.css'
 
 class UserDetailPage extends Component {
-
-  componentWillMount () {
+  componentWillMount() {
     const {id} = this.props.match.params
     this.props.getUser(id)
   }
 
-  render () {
+  render() {
+    const saved = null
     const {match} = this.props
     const user = this.props.user ? this.props.user._source : null
-    const links = [
-      {type: 'github', label: 'Github Profile', component: <GithubIcon />},
-      {type: 'linkedin', label: 'LinkedIn Profile', component: <LinkedInIcon />},
-      {type: 'twitter', label: 'Twitter Profile', component: <TwitterIcon />},
-      {type: 'personal_site', label: 'Personal Site', component: <LinkIcon />}
-    ]
-    let icons = []
-    if (user) {
-      links.forEach(link => {
-        if (user[link.type]) {
-          icons.push({
-            text: link.label,
-            component: link.component,
-            url: user[link.type]
-          })
-        }
-      })
-    }
-    // below we're fixing the unnecessary padding when this component
-    // is rendered by the applicant dashboard
     let paddingTop = match.path === '/users/:id' ? '60px' : '0'
     return (
-      <Row className='UserDetail Dashboard__content--white' style={{paddingTop}}>
+      <Row className='JobInfo Dashboard__content--white' style={{paddingTop}}>
         {user &&
         <Col xs={12} sm={12} md={12} lg={12}>
-          <Row className='UserDetail__header'>
+          <Row className='JobInfo--header'>
             <Col xs={12} sm={12} md={12} lg={12}>
               <Row>
-                <Col xs={12} sm={12} md={3} lg={3}>
-                  <Image
-                    className='UserDetail__header-avatar'
-                    circle
-                    responsive
-                    src={user.image_url ? user.image_url : blankAvatar}
-                    alt={`${user.first_name}'s avatar`}
-                  />
+                <Image
+                className='user-avatar'
+                circle
+                responsive
+                src={user.image_url ? user.image_url : blankAvatar }
+                alt={`${user.first_name}'s' avatar`}
+                />
+                <Col className='header-left' xs={12} sm={6} md={6} lg={6}>
+                  <h5 className='JobInfo--header-employer'>{user.first_name} {user.last_name}</h5>
+                  <p className='JobInfo--header-location'>{`${user.location}`}</p>
                 </Col>
-                <Col className='header-left' xs={12} sm={12} md={5} lg={5}>
-                  <h1 className='UserDetail__header-name'>
-                    {`${user.first_name} ${user.last_name}`}
-                  </h1>
-                  <IconBar icons={icons} />
-                  <p>{user.headline}</p>
-                </Col>
-                <Col className='chip-container' xs={12} sm={12} md={4} lg={4}>
-                  <div className='header-right'>
-                    <p className='UserDetail__header-title'>{user.title}</p>
-                    <p className='UserDetail__header-location'>
-                      <Glyphicon glyph='globe' /> {`${user.location}`}
-                    </p>
-                    {
-                      <Chips
-                        type='round'
-                        words={user.employment_types || []}
-                        justify='center'
-                        align='flex-start'
-                      />
-                    }
-                  </div>
+                <Col className='header-right' xs={12} sm={6} md={3} mdOffset={3} lg={3} lgOffset={3}>
+                  <h5 className='JobInfo--header-payrate'>
+                    {user.personal_site}
+                  </h5>
+                  {user.employment_types && user.employment_types.map((type, i) => (
+                    <span key={i} className='JobInfo--header-type'>{type}</span>
+                  ))}
                 </Col>
               </Row>
             </Col>
           </Row>
-          <Row className='UserDetail__body'>
+          <Row className='JobInfo--body'>
             <Col xs={12} sm={12} md={12} lg={12}>
               <Row>
-                <Col className='UserDetail__projects' xs={12} sm={7} md={8} lg={8}>
-                  <h2>PROJECTS</h2>
-                  <section className='UserDetail__project-section'>
-
-                    <ul>
-                      {user.projects && user.projects.map((project, i) => (
-                        <li key={i} className='UserDetail__header-type'>
-                          <h3>{project.title}</h3>
-                          <p className='JobCard-skills'>{project.skills.map(skill => skill.title).join(', ')}</p>
-                          <h3>Project Link</h3>
-                          <p>{project.external_link}</p>
-                          <h3>Description</h3>
-                          <p>{project.description}</p>
-                          <h3>Learning Point</h3>
-                          <p>{project.learning_point}</p>
-                          <h3>Learning Point</h3>
-                          <p>{project.pain_point}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
-                </Col>
+                <div className='container__flex--sidebar'>
+                  <Col className='JobInfo--summary' xs={12} sm={7} md={8} lg={8}>
+                    <section className='JobInfo--summary-section'>
+                      <h2>Projects</h2>
+                      <ul>
+                        {user.projects && user.projects.map((project, i) => (
+                          <li key={i} className='JobInfo--header-type'>
+                            <h3>{project.title}</h3>
+                            <p className='JobCard-skills'>{project.skills.map(skill => skill.title).join(', ')}</p>
+                            <h5>Project Link</h5>
+                            <p>{project.external_link}</p>
+                            <h5>Description</h5>
+                            <p>{project.description}</p>
+                            <h5>Learning Point</h5>
+                            <p>{project.learning_point}</p>
+                            <h5>Learning Point</h5>
+                            <p>{project.pain_point}</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </section>
+                  </Col>
+                  <Col className='JobInfo--sidebar' xs={12} sm={5} md={4} lg={4}>
+                    <Image
+                    className='user-avatar'
+                    circle
+                    responsive
+                    src={user.resume_url ? user.resume_url : blankAvatar }
+                    alt={`${user.first_name}'s' resume`}
+                    />
+                    <Button
+                      className='btn-oval btn-oval__black'
+                      onClick={!saved ? this.saveJob : this.unsaveJob}
+                    >
+                      {!saved ? 'SAVE' : 'UNSAVE'}
+                    </Button>
+                    <div className='JobInfo--subscribe-container'>
+                      <h4>
+                        Reach out to this user!
+                      </h4>
+                      <form>
+                        <ControlLabel srOnly>Email</ControlLabel>
+                        <FormControl
+                          type='email'
+                          placeholder='EMAIL'
+                          onChange={this.handleChange}
+                        />
+                        <Button type='submit' className='JobInfo--subscribe-button'>
+                          SEND
+                        </Button>
+                      </form>
+                    </div>
+                  </Col>
+                </div>
               </Row>
             </Col>
           </Row>
@@ -120,19 +112,20 @@ class UserDetailPage extends Component {
   }
 }
 
+// {skills && skills.map((skill, i) => (
+//   <span key={i} className='skill-chip'>{skill}</span>
+// ))}
+
 UserDetailPage.propTypes = {
-  history: PropTypes.object,
-  match: PropTypes.object,
-  getUser: PropTypes.func,
-  user: PropTypes.object
+  history: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-  user: state.users.selected
+  user: state.users.selected,
 })
 
 const mapDispatchToProps = dispatch => ({
-  getUser: id => dispatch(gettingUserById(id))
+  getUser: job_id => dispatch(gettingUserById(job_id)),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserDetailPage)
