900bce4d0fc4e0a11c75ae21bbc81d792d209318
Chloe Rice <chloe.mi.rice@gmail.com>
2017-08-25 19:39:41 -0700

    user profile almost done - project detail modal next

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -14,13 +14,28 @@ import './UserProfile.css'
 
 class UserProfile extends Component {
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      opacity: '0',
+      showModal: false
+    }
+  }
+
   componentWillMount () {
     const {id} = this.props.match.params
     if (!this.props.user) this.props.getUser(id)
   }
 
+  handleOnLoad = () => {
+    this.setState({opacity: '1'})
+  }
+
+  handleClick = () => {
+    this.setState({showModal: !this.state.showModal})
+  }
+
   render () {
-    const {match, showDashMenu} = this.props
     const user = this.props.user ? this.props.user._source : null
     const links = [
       {type: 'github', label: 'Github Profile', component: <GithubIcon />},
@@ -40,76 +55,106 @@ class UserProfile extends Component {
         }
       })
     }
-
+    const {opacity} = this.state
     return (
-      <Row className='UserDetail Dashboard__content--white'>
+      <Row className='UserDetail'>
         {user &&
         <Col xs={12} sm={12} md={12} lg={12}>
           <Row className='UserDetail__header'>
             <Col xs={12} sm={12} md={12} lg={12}>
               <Row>
-                <Col xs={12} sm={12} md={3} lg={3}>
+                <Col xs={12} sm={12} md={2} lg={2}>
                   <Image
                     className='UserDetail__header-avatar'
                     circle
+                    style={{opacity}}
                     responsive
+                    onLoad={this.handleOnLoad}
                     src={user.image_url ? user.image_url : blankAvatar}
                     alt={`${user.first_name}'s avatar`}
                   />
                 </Col>
-                <Col className='header-left' xs={12} sm={12} md={5} lg={5}>
-                  <h1 className='UserDetail__header-name'>
-                    {`${user.first_name} ${user.last_name}`}
-                  </h1>
-                  <IconBar icons={icons} />
-                  <p>{user.headline}</p>
+                <Col className='header-left-container' xs={12} sm={8} md={7} lg={7}>
+                  <div className='header-left'>
+                    <h1 className='UserDetail__header-name'>
+                      {`${user.first_name} ${user.last_name}`}
+                    </h1>
+                    <p className='UserDetail__header-headline'>
+                      This is your headline, a short elevator pitch highlighting a key goal or accomplishment, written in your voice so your personality is front and center.
+                    </p>
+                  </div>
                 </Col>
-                <Col className='chip-container' xs={12} sm={12} md={4} lg={4}>
+                <Col className='header-right-container' xs={12} sm={4} md={3} lg={3}>
                   <div className='header-right'>
-                    <p className='UserDetail__header-title'>{user.title}</p>
+                    <p className='UserDetail__header-title'>
+                      {user.title}
+                    </p>
                     <p className='UserDetail__header-location'>
                       <Glyphicon glyph='globe' /> {`${user.location}`}
                     </p>
-                    {
-                      <Chips
-                        type='round'
-                        words={user.employment_types || []}
-                        justify='center'
-                        align='flex-start'
-                      />
-                    }
+                    <IconBar icons={icons} />
                   </div>
                 </Col>
               </Row>
             </Col>
           </Row>
           <Row className='UserDetail__body'>
-            <Col xs={12} sm={12} md={12} lg={12}>
-              <Row>
-                <Col className='UserDetail__projects' xs={12} sm={7} md={8} lg={8}>
-                  <h2>PROJECTS</h2>
-                  <section className='UserDetail__project-section'>
-
-                    <ul>
-                      {user.projects && user.projects.map((project, i) => (
-                        <li key={i} className='UserDetail__header-type'>
-                          <h3>{project.title}</h3>
-                          <p className='JobCard-skills'>{project.skills.map(skill => skill.title).join(', ')}</p>
-                          <h3>Project Link</h3>
-                          <p>{project.external_link}</p>
-                          <h3>Description</h3>
-                          <p>{project.description}</p>
-                          <h3>Learning Point</h3>
-                          <p>{project.learning_point}</p>
-                          <h3>Learning Point</h3>
-                          <p>{project.pain_point}</p>
-                        </li>
-                      ))}
-                    </ul>
-                  </section>
-                </Col>
-              </Row>
-            </Col>
+            <div className='container__flex'>
+              <Col className='UserDetail__body-section' xs={12} sm={9} md={8} lg={8}>
+                <div className='portfolio'>
+                  <h2 className='UserDetail__body-header'>
+                    Portfolio
+                  </h2>
+                  <Row>
+                    {
+                      user.projects && user.projects.map(p => {
+                        const skills = p.skills.map(s => s.title)
+                        return (
+                          <Col className='portfolio-card-container' xs={12} sm={6} md={6} lg={6}>
+                            <button className='portfolio-card' onClick={this.handleClick}>
+                              <div className='screenshot-placeholder'>
+                                <Image
+                                  style={{opacity}}
+                                  className='UserDetail__project-screenshot'
+                                  src={blankAvatar}
+                                  alt={`Screenshot of ${p.title}`}
+                                  responsive
+                                  onLoad={this.handleOnLoad}
+                                />
+                              </div>
+                              <div className='portfolio-card__text'>
+                                <h4>{`${p.title}`}</h4>
+                                <Chips
+                                  words={skills}
+                                  type={'round-bordered'}
+                                  align={'center'}
+                                  justify={'flex-start'}
+                                  margin={'10px 0'}
+                                />
+                              </div>
+                            </button>
+                          </Col>
+                        )
+                      })
+                    }
+                  </Row>
+                </div>
+              </Col>
+              <Col className='UserDetail__body-section' xs={12} sm={3} md={4} lg={4}>
+                <div className='summary'>
+                  <h2 className='UserDetail__body-header text-white'>Bio</h2>
+                  <p className='summary-text'>
+                    Expand on your tagline with a summary of your education or self-taught journey and experience.
+                  </p>
+                  <p className='summary-text'>
+                    What are your specialities? What are you most interested in working on? What are you learning now?
+                  </p>
+                  <p className='summary-text'>
+                    Give the employer a glimpse of who you are, both as a tech professional and as a human who cares about more than technology.
+                  </p>
+                </div>
+              </Col>
+            </div>
           </Row>
         </Col>
       }
--- src/components/utilities/chips/Chips.js
+++ src/components/utilities/chips/Chips.js
@@ -2,8 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Chips.css'
 
-const Chips = ({words, type, align, justify}) => (
-  <div className='chip-wrapper' style={{alignItems: align, justifyContent: justify}}>
+const Chips = ({words, type, align, justify, margin}) => (
+  <div
+    className='chip-wrapper'
+    style={{
+      margin,
+      alignItems: align,
+      justifyContent: justify
+    }}
+  >
     {
       words.map((word, i) => (
         <span key={i} className={`chip-${type}`}>
@@ -18,7 +25,8 @@ Chips.propTypes = {
   words: PropTypes.arrayOf(PropTypes.string),
   align: PropTypes.string, // any of the align-items flexbox property options
   justify: PropTypes.string, // any of the justify-content flexbox property options
-  type: PropTypes.string // either round or square, used to interpolate className
+  type: PropTypes.string, // either round or square, used to interpolate className
+  margin: PropTypes.string // the margin for aligning the chips (e.g., '0 auto', or '10px 0')
 }
 
 export default Chips
